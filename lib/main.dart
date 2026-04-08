5eb8b51522ae060c6c8aa116dc14e2f0de7f468a
akramnemri <florensiahiro1@gmail.com>
2026-04-08 23:10:15 +0200

    fix: correct catalogue_screen import path in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'firebase_options.dart';
-import 'features/auth/presentation/login_screen.dart';
+import 'features/auth/presentation/catalogue_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
         useMaterial3: true,
       ),
-      home: const LoginScreen(),
+home: const CatalogueScreen(),
     );
   }
 }
